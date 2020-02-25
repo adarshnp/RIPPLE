9ff26346cbec33276c83fdb901c90e91ad9a0371
adarshnp <adarshnp49@gmail.com>
2020-02-25 15:41:38 +0530

    ball movement done

---
+++ RIPPLE/Assets/Ripple.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ripple : MonoBehaviour
+{
+	public GameObject ripple;
+	void Update()
+	{
+		if(Input.touchCount > 0)
+		{
+			Touch touch = Input.GetTouch(0);
+			Vector2 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
+			if(touch.phase == TouchPhase.Began)
+			{
+				Instantiate (ripple,touchPos,Quaternion.Identity);
+			}
+		}
+	}
+ }
